ddd6a72b6f4cedfffb387e7d111b236b2c6b574a
Newproject <talapalaparthasarathi9@gmail.com>
2026-03-01 11:04:34 +0530

    init

--- groceryproject/settings.py
+++ groceryproject/settings.py
@@ -98,3 +98,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
